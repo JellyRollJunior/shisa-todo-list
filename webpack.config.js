cf76b19eee884d65359aa3a9e586c241cd4b682d
Brandon Lin <brandonlin9716@gmail.com>
2024-09-12 18:42:19 -0700

    Setup initial html, css, js files

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   devtool: "eval-source-map",
   devServer: {
-    watchFiles: ["./src/template.html"],
+    watchFiles: ["./src/index.html"],
   },
   plugins: [
     new HtmlWebpackPlugin({
